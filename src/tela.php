a5d17726abcd6d935dc5e33ce099b4d47683db8d
marcos-inja <marcosvaas@gmail.com>
2021-07-21 15:37:04 -0300

    Tela

--- src/tela.php
+++ src/tela.php
@@ -4,11 +4,8 @@ include('verificar.php');
 include('conexao.php');
 
 $query = "select * from usuario";
-
 $result = mysqli_query($conexao, $query);
-
 $row = mysqli_num_rows($result);
-
 ?>
 
 <html lang="pt-br">
@@ -17,38 +14,44 @@ $row = mysqli_num_rows($result);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/tela.css">
     <title>Tela</title>
 </head>
 
 <body>
-<?php
 
-if ($result->num_rows > 0) {
-    echo 'contas:';
-    echo '<br>';
-    while ($linha = $result->fetch_assoc()) {
-        
-        echo '<div class="card bg-light mb-3" style="max-width: 18rem;">';
-        echo '<div class="card-header">@'. $linha['usuario'].'</div>';
-        echo '<div class="card-body">';
-        echo '<h5 class="card-title">'.$linha['nome'].'</h5>';
-        echo '</div>';
-        echo '</div>'; 
-    
-    }
-}
-?>
-    
+    <div class="conteiner">
+        <div class="dados">
+            <h2>Marcos Vinicius</h2>
+        </div>
+
+        <div class="contas">
+            <?php
+            if ($result->num_rows > 0) {
+                echo '<h1>contas:</h1>';
+                echo '<br>';
+                while ($linha = $result->fetch_assoc()) {
+                    
+                    echo '<div class="card"><h4>@' . $linha['usuario'] . '</h4><hr><h6>' . $linha['nome'] .'</h6><h6>' . $linha['nome'] .'</h6></div>';
+                }
+            }
+            ?>
+        </div>
+
+        <div class="usuario">
+            <h2>Usúario logado: <?php echo $_SESSION['nome']; ?></h2>
+            <h2><a href="logout.php">Sair</a></h2>
+
+            <?php
+            if ($row == 1) {
+                $usuario_bd = mysqli_fetch_assoc($result);
+                $_SESSION['nome'] = $usuario_bd['nome'];
+            }
+
+            ?>
+        </div>
+    </div>
 </body>
 
 </html>
 
-<h2>Usúario logado: <?php echo $_SESSION['nome']; ?></h2>
-
-<h2><a href="logout.php">Sair</a></h2>
-<?php
-if ($row == 1) {
-    $usuario_bd = mysqli_fetch_assoc($result);
-    $_SESSION['nome'] = $usuario_bd['nome'];
-}
-?>
